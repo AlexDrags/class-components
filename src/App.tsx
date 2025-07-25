cb2e8e806b0f15c06ec80f19211d544b93b7eb2c
AlexDrags <ksander-88@yandex.ru>
2025-07-25 19:52:20 +0300

    feat:add logic local storage to hook useEffect to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import { getDataPrev } from './api/getData';
-// import { useState, type ChangeEvent, type FormEvent } from 'react';
 import Header from './components/Header/Header';
 import ErrorBoundary from './components/Error/Error';
 import CardList from './components/CardList/CardList';
-// import Main from './components/Main/Main';
-// import CardList from './components/CardList/CardList';
-// import { getDataPrev, getDataNext, getError } from './api/getData';
-// import { Pagination } from './components/Pagination/Pagination';
-// import { ErrorButton } from './components/ErrorButton/ErrorButton';
-// import ErrorBoundary from './components/Error/Error';
-// import { useState } from 'react';
-// import searchData from './api/search';
-// import saveSearchState from './api/saveSearchState';
+import Main from './components/Main/Main';
+import searchData from './api/search';
+import { Pagination } from './components/Pagination/Pagination';
 
 interface ICard {
   name: string;
@@ -26,60 +19,18 @@ export default function App() {
   const [universities, setUniversities] = useState<ICard[]>([]);
   useEffect(() => {
     async function fetchData() {
-      const response = await getDataPrev();
-      setUniversities(response);
+      if (!localStorage.getItem('lastSearch')) {
+        const response = await getDataPrev();
+        setUniversities(response);
+      }
+      if (localStorage.getItem('lastSearch')) {
+        const lastQuery = localStorage.getItem('lastSearch');
+        const lastData = await searchData(`${lastQuery}`);
+        setUniversities(lastData);
+      }
     }
     fetchData();
   }, []);
-  // constructor(props: object) {
-  //   super(props);
-  //   this.state = { value: '', universities: [] };
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  //   this.handlePaginationPrev = this.handlePaginationPrev.bind(this);
-  //   this.handlePaginationNext = this.handlePaginationNext.bind(this);
-  //   this.handleError = this.handleError.bind(this);
-  // }
-
-  // async componentDidMount() {
-  //   if (!localStorage.getItem('lastSearch')) {
-  //     this.setState({
-  //       universities: await getDataPrev(),
-  //     });
-  //   }
-  //   if (localStorage.getItem('lastSearch')) {
-  //     const lastQuery = localStorage.getItem('lastSearch');
-  //     this.setState({
-  //       universities: await searchData(`${lastQuery}`),
-  //     });
-  //   }
-  // }
-
-  // async handleChange(event: ChangeEvent<HTMLInputElement>) {
-  //   this.setState({ value: event.target.value });
-  //   saveSearchState(event.target.value);
-  // }
-
-  // async handleError(event: FormEvent<HTMLFormElement>) {
-  //   event.preventDefault();
-  //   this.setState({
-  //     universities: await getError(),
-  //   });
-  // }
-
-  // async handlePaginationPrev(event: FormEvent<HTMLFormElement>) {
-  //   event.preventDefault();
-  //   this.setState({
-  //     universities: await getDataPrev(),
-  //   });
-  // }
-
-  // async handlePaginationNext(event: FormEvent<HTMLFormElement>) {
-  //   event.preventDefault();
-  //   this.setState({
-  //     universities: await getDataNext(),
-  //   });
-  // }
 
   return (
     <>
@@ -90,19 +41,15 @@ export default function App() {
           universities={universities}
           setUniversities={setUniversities}
         />
-        <CardList universities={universities} />
-      </ErrorBoundary>
-      {/* <ErrorBoundary>
         <Main>
           <h2>Universities</h2>
-
+          <CardList universities={universities} />
           <Pagination
-            handlePaginationPrev={this.handlePaginationPrev}
-            handlePaginationNext={this.handlePaginationNext}
+            handlePaginationPrev={() => {}}
+            handlePaginationNext={() => {}}
           />
-          <ErrorButton handleError={this.handleError} />
         </Main>
-      </ErrorBoundary> */}
+      </ErrorBoundary>
     </>
   );
 }
