1cae5729dbf88b1d8a0297073ec43651832d3ab1
AlexDrags <ksander-88@yandex.ru>
2025-07-31 20:31:21 +0300

    feat:add logic save info to the csv file

--- src/components/FlyoutElement/FlyoutElement.tsx
+++ src/components/FlyoutElement/FlyoutElement.tsx
@@ -1,16 +1,30 @@
 import './style.css';
+import { CSVLink } from 'react-csv';
 import { useStoreStateCheckCards } from '../../store/store';
 export default function FlyoutElement() {
   const clearCheckedCards = useStoreStateCheckCards(
     (state) => state.clearCheckedCards
   );
   const checkedCards = useStoreStateCheckCards((state) => state.checkedCards);
+  const headers = [
+    { label: 'Name of universities', key: 'name' },
+    { label: 'Country', key: 'country' },
+    { label: 'Web-page', key: 'web_pages' },
+  ];
   return (
     <div className="modal">
       <p>{`${checkedCards.length}`} items are selected</p>
       <div>
         <button onClick={clearCheckedCards}>Unselect all</button>
-        <button onClick={() => {}}>Download in cvs</button>
+        <CSVLink
+          data={checkedCards}
+          filename={`${checkedCards.length}_items.csv`}
+          separater={';'}
+          headers={headers}
+        >
+          Download cvs
+        </CSVLink>
+        ;
       </div>
     </div>
   );
