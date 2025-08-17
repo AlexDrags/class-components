5aa942cda6b920e216e8fe880ffea10e521ed9c9
AlexDrags <ksander-88@yandex.ru>
2025-08-18 00:46:39 +0300

    fix:work not found page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
 export default function NotFound() {
+  const t = useTranslations('NotFound');
   return (
     <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <h2>{t('title')}</h2>
+      <p>{t('description')}</p>
     </div>
   );
 }
